448b5598a1bcce3eb05354a86bb3a7feab373a84
stuart-belcke <stuart-belcke@users.noreply.github.com>
2026-07-29 13:30:40 +0000

    Deploy preview for PR 28 🛫

---
+++ pr-preview/pr-28/assets/index.esm-B8TNC1py.js
@@ -0,0 +1,16 @@
+import{i as p,r as e}from"./index.esm2017-Crwo03JA.js";import{F as _,S as t,D as n,_ as m,a as v,b as A,c as g,d as E,e as b,f as d,g as f}from"./index.esm2017-Crwo03JA.js";var r="firebase",s="11.10.0";/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */e(r,s,"app");export{_ as FirebaseError,t as SDK_VERSION,n as _DEFAULT_ENTRY_NAME,m as _addComponent,v as _apps,A as _components,g as _getProvider,E as _isFirebaseServerApp,b as _registerComponent,d as _serverApps,f as getApp,p as initializeApp,e as registerVersion};
